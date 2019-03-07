a790eb36838053499990bfcda60d5765ffb64053
Alexander Handstanger <zombeyfan.ah@gmail.com>
2019-03-07 11:13:25 +0100

    it builds

--- bst.cpp
+++ bst.cpp
@@ -10,3 +10,65 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include <stdlib.h>
+#include "bst.h"
+#include "general.h"
+
+struct Node{
+  int value;
+  struct Node* right;
+  struct Node* left;
+};
+
+
+Bst new_bst(){
+  return 0;
+}
+
+void delete_bst(Bst bst){
+
+}
+
+int get_depth(Bst bst){
+  return 0;
+}
+
+void add(Bst* bst, int value){
+
+}
+
+int root_value(Bst bst){
+  return 0;
+}
+
+Bst left_subtree(Bst root){
+  return 0;
+}
+
+Bst right_subtree(Bst root){
+  return 0;
+}
+
+int traverse_pre_order(Bst bst, int *elements, int start){
+  return 0;
+}
+
+int traverse_in_order(Bst bst, int *elements, int start){
+  return 0;
+}
+
+int traverse_post_order(Bst bst, int *elements, int start){
+  return 0;
+}
+
+bool are_equal(Bst bst1, Bst bst2){
+  return false;
+}
+
+void most_left_longest_branch(Bst bst, Bst* branch){
+
+}
+
+int get_number_of_subtrees(Bst bst){
+  return 0;
+}
